dcf6f5e6037d148447a59b5a203536156ba376ba
LucNieto <noodle3030@hotmail.com>
2019-01-20 19:21:53 -0600

    Initian Django configurations

--- ZillowClone/settings.py
+++ ZillowClone/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+   # 'Pages.apps.ZillowConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Zillow.apps.ZillowConfig',
+    'Pages.apps.ZillowConfig',
 ]
 
 MIDDLEWARE = [
