e777a8d32363af0bf4968af26d769121139de702
LucNieto <noodle3030@hotmail.com>
2019-01-20 19:38:33 -0600

    setting up root page

--- ZillowClone/settings.py
+++ ZillowClone/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-   # 'Pages.apps.ZillowConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
